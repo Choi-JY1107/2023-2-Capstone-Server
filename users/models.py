7c7777b202713133e5ca1658c03201a44987160e
Choi-JY <Choi-JY1107@github.com>
2023-10-10 20:00:49 +0900

    fix: User 모델 재 수정

--- users/models.py
+++ users/models.py
@@ -6,8 +6,7 @@ class User(AbstractUser):
     nickname = models.CharField(verbose_name="별명", max_length=8, null=False, unique=True)
     password = models.CharField(verbose_name="패스워드", max_length=20)
     phone_number = models.CharField(max_length=15, null=True)
-    register_date = models.DateTimeField(auto_now_add=True, null=True)
+    register_date = models.DateTimeField(auto_now_add=True, null=False)
 
-    # profile_image = models.ImageField(
-    #     "프로필 이미지", upload_to="users/profile", blank=True)
-    # short_description = models.TextField("소개글", blank=True)
+    class Meta:
+        db_table = 'Users'
