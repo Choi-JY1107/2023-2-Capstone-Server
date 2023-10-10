5f94a67aa0ea1cc02dd9956021fd6cc0d3fb82c8
Choi-JY <Choi-JY1107@github.com>
2023-10-10 18:33:59 +0900

    fix: User 모델 세부 사항 변경

--- users/models.py
+++ users/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    nickname = models.CharField(verbose_name="별명", max_length=8, null=True)
-    password = models.CharField(verbose_name="패스워드", max_length=32)
+    nickname = models.CharField(verbose_name="별명", max_length=8, null=False, unique=True)
+    password = models.CharField(verbose_name="패스워드", max_length=20)
     phone_number = models.CharField(max_length=15, null=True)
     register_date = models.DateTimeField(auto_now_add=True, null=True)
 
