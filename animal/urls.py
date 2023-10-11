9b92f44e3934b82068d3df44fc2fbff000b89da4
Choi-JY <Choi-JY1107@github.com>
2023-10-12 02:50:50 +0900

    fix: animal 생성 url 변경

--- animal/urls.py
+++ animal/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from animal.views import CreateAnimalAPI
 
 urlpatterns = [
-    path("test/", CreateAnimalAPI.as_view()),
+    path("create/", CreateAnimalAPI.as_view()),
+    # path("(?P<pk>\d+/$", )
 ]
