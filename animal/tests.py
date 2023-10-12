1f30180b2eb1e0e6b0f1e6def571748025af25a5
Choi-JY <Choi-JY1107@github.com>
2023-10-12 15:59:50 +0900

    test: animal 정보 테스트 코드 작성

--- animal/tests.py
+++ animal/tests.py
@@ -19,23 +19,32 @@ class AnimalCreateViewTestCase(TestCase):
         data = {"nickname": "test"}
         response = self.client.post(path, data, headers=self.headers)
 
-        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
 
 
 class AnimalInfoViewTestCase(TestCase):
-    def setUpTestData(self):
-        self.login_path = "/users/login/"
-        self.login_data = {"username": "testUser", "password": "testUser"}
-        self.animal_path = "/animal/create/"
-        self.animal_data = {"nickname": "test"}
-        self.info_path = "/animal/info/1"
-
     def setUp(self):
-        login_response = self.client.post(self.login_path, self.login_data)
+        self.animal_data = {"nickname": "test"}
+        User.objects.create_user(username="testUser", password="testUser")
+        Animal.create_animal(data=self.animal_data, user=User.objects.get(username="testUser"))
+
+        login_path = "/users/login/"
+        login_data = {"username": "testUser", "password": "testUser"}
+        login_response = self.client.post(login_path, login_data)
+
         token = login_response.json()['token']
         self.headers = {'Authorization': 'Bearer ' + token}
-        self.client.post(self.animal_path, self.animal_data, headers=self.headers)
 
     def test_animal_info_view_can_find_instance(self):
-        info_response = self.client.get(path=self.info_path)
+        info_path = "/animal/info/1"
+        info_response = self.client.get(path=info_path, headers=self.headers)
         self.assertEquals("test", info_response.json()['nickname'])
+
+# class AnimalImageCreateViewTestCase(TestCase):
+#     def setUpTestData(self):
+#         self.login_path = "/users/login/"
+#         self.login_data = {"username": "testUser", "password": "testUser"}
+#         self.animal_path = "/animal/create/"
+#         self.animal_data = {"nickname": "test"}
+#         self.image_path = "/animal/image/"
+#         self.image_data = {""}
