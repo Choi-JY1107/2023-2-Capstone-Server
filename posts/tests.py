15c668bb47c57667925c37e0e11a467345cc01ce
Choi-JY <Choi-JY1107@github.com>
2023-11-06 21:20:21 +0900

    test: Post 생성 테스트 코드 작성

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,40 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from rest_framework import status
 
-# Create your tests here.
+from posts.models import Post
+from users.models import User
+
+
+class PostInstanceTestCase(TestCase):
+    def setUp(self):
+        User.objects.create_user(username='testUser', password='testUser')
+        login_path = "/users/login/"
+        login_data = {"username": "testUser", "password": "testUser"}
+        login_response = self.client.post(login_path, login_data)
+        token = login_response.json()['token']
+        self.headers = {'Authorization': 'Bearer ' + token}
+
+    def test_post_can_create_instance(self):
+        user = User.objects.get(id=1)
+        post = Post.objects.create(
+            content='안녕하쇼',
+            register_id=user
+        )
+        self.assertEquals(post.content, '안녕하쇼')
+
+
+class PostCreateViewTestCase(TestCase):
+    def setUp(self):
+        User.objects.create_user(username='testUser', password='testUser')
+        login_path = "/users/login/"
+        login_data = {"username": "testUser", "password": "testUser"}
+        login_response = self.client.post(login_path, login_data)
+        token = login_response.json()['token']
+        self.headers = {'Authorization': 'Bearer ' + token}
+
+    def test_post_view_can_create_instance(self):
+        path = "/posts/create/"
+        data = {"content": "안녕하쇼"}
+        response = self.client.post(path, data, headers=self.headers)
+
+        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.views import APIView
+
+from posts.models import Post
 
 
-def feeds(request):
-    if not request.user.is_authenticated:
-        return redirect("/users/login/")
-    return render(request, "posts/feeds.html")
+# animal 관련
+class CreateAnimalAPI(APIView):
+
+    @staticmethod
+    def post(request):
+        try:
+            animal = Post.create_animal(data=request.data, user=request.user)
+            return JsonResponse(
+                data={'message': "id = %d인 User 가 id = %s인 Animal을 등록하였습니다." % (request.user.id, animal)},
+                status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return JsonResponse(data={'message': str(e)}, status=status.HTTP_400_BAD_REQUEST)
