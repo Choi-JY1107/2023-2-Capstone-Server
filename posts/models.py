56dfe5986b8b7ee08394eecaf60d4d385b601f67
Choi-JY <Choi-JY1107@github.com>
2023-11-06 20:58:59 +0900

    feat: 게시판 모델 생성

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+from animal.models import AnimalImage
+
+
+class Post(models.Model):
+    like_count = models.PositiveIntegerField(default=0)
+    content = models.CharField(max_length=1000, null=True)
+    register_id = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    register_date = models.DateTimeField(auto_now_add=True, null=False)
+
+    class Meta:
+        db_table = 'Animal'
+
+    def __str__(self):
+        return str(self.id)
+
+
+class PostImage(models.Model):
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE, null=False)
+    image_id = models.ForeignKey(AnimalImage, on_delete=models.CASCADE, null=False)
+    register_data = models.DateTimeField(auto_now_add=True, null=False)
+
+
+class PostLike(models.Model):
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE, null=False)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    register_data = models.DateTimeField(auto_now_add=True, null=False)
