088ee043051aefaf95903818aa703c1c3d06bc14
Choi-JY <Choi-JY1107@github.com>
2023-11-13 21:56:42 +0900

    feat: 실종 신고 시 알림 기능 구현

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from posts.views import CreatePostAPI, CreateMissingAPI, ListMissingAPI
+from posts.views import (CreatePostAPI, CreateMissingAPI, ListMissingAPI,
+                         AlertMissingAPI)
 
 urlpatterns = [
     path("create/", CreatePostAPI.as_view()),
     path("missing/create/", CreateMissingAPI.as_view()),
     path("missing/list/", ListMissingAPI.as_view()),
+    path("missing/alert/", AlertMissingAPI.as_view()),
 ]
--- posts/views.py
+++ posts/views.py
@@ -4,9 +4,11 @@ from rest_framework.views import APIView
 
 from posts.models import Post, MissingImage
 from posts.serializers import MissingListSerializer
+from animal.models import Animal
+from users.models import UserDevice
+from util.send_to_firebase_cloud_messaging import send_to_firebase_cloud_messaging
 
 
-# Post 관련
 class CreatePostAPI(APIView):
 
     @staticmethod
@@ -39,6 +41,31 @@ class ListMissingAPI(APIView):
         try:
             missing = MissingImage.objects.all()
             serializer = MissingListSerializer(missing, many=True)
-            return JsonResponse(data={"data": serializer.data, "message": "Missing List Success"}, status=200)
+            return JsonResponse(data={"data": serializer.data, "message": "Missing List Success"}, status=status.HTTP_200_OK)
         except Exception as e:
             return JsonResponse(data={'message': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class AlertMissingAPI(APIView):
+    @staticmethod
+    def post(request):
+        try:
+            animal_id = request.data['animal_id']
+            animal = Animal.objects.get(id=animal_id)
+            if animal.owner != request.user :
+                raise Exception("It is not your animal")
+            animal.is_missing = True
+
+            # TODO 사용자 위치 정보에 알맞는 사람만 알림 보내는 로직 추가
+            title = "실종 신고"
+            body = f"'{animal.nickname}' 이름을 가진 동물을 찾습니다."
+            link = "http:127.0.0.1:8000/login"
+
+            device_list = UserDevice.objects.all()
+            for device in device_list :
+                send_to_firebase_cloud_messaging(device.fcm_token, title, body, link)
+
+            return JsonResponse(data={"message": f"{len(device_list)}개의 디바이스에 알림 전송 완료"}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return JsonResponse(data={'message': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
