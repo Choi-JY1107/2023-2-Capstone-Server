5571a956a8e9b687b85d48c8dab8deea319b3267
Choi-JY <Choi-JY1107@github.com>
2023-11-13 20:31:30 +0900

    feat: userdevice table admin 추가

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from users.models import User
+from users.models import User, UserDevice
 
 
 @admin.register(User)
@@ -9,3 +9,13 @@ class CustomUserAdmin(UserAdmin):
         'id', 'nickname', 'phone_number', 'register_date'
     )
     search_fields = ['id', 'nickname']
+
+
+class UserDeviceAdmin(admin.ModelAdmin):
+    list_display = (
+        'user_id', 'fcm_token'
+    )
+    search_fields = ['user_id']
+
+
+admin.site.register(UserDevice, UserDeviceAdmin)
