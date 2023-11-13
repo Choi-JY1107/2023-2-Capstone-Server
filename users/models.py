002d2526f83874e70198a27f5d9fee6c58880380
Choi-JY <Choi-JY1107@github.com>
2023-11-13 18:56:46 +0900

    feat: 푸시 알림 table 추가

--- users/models.py
+++ users/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
 class User(AbstractUser):
     nickname = models.CharField(verbose_name="별명", max_length=8, null=False, unique=True)
     password = models.CharField(verbose_name="패스워드", max_length=20)
@@ -13,3 +12,12 @@ class User(AbstractUser):
 
     def __str__(self):
         return str(self.id)
+
+class UserDevice(models.Model):
+    user_id = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name="", null=True
+    )
+    fcm_token = models.CharField("FCM Token", blank=True, max_length=500, null=True)
+
+    class Meta:
+        db_table = 'User_Device'
