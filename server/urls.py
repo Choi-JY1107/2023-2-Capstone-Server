6ceadf69f14f84b4b9b399c8c285478159ba6ac2
Choi-JY <Choi-JY1107@github.com>
2023-11-14 21:16:00 +0900

    fix: animal url을 animals로 변경

--- server/urls.py
+++ server/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
     path("posts/", include("posts.urls")),
-    path("animal/", include("animal.urls")),
+    path("animals/", include("animal.urls")),
     path("", index),
 ]
 
