7aa996df62fd0396047107dd7fc327b5d0c54757
Choi-JY <Choi-JY1107@github.com>
2023-11-14 21:00:52 +0900

    fix: username 최대 16글자로 변경

--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    nickname = models.CharField(verbose_name="별명", max_length=8, null=False, unique=True)
+    nickname = models.CharField(verbose_name="별명", max_length=16, null=False, unique=True)
     password = models.CharField(verbose_name="패스워드", max_length=20)
     phone_number = models.CharField(max_length=15, null=True)
     register_date = models.DateTimeField(auto_now_add=True, null=False)
